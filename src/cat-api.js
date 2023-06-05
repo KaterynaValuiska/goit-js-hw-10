74bed9ba8b4dd412d83fe5f004ce1f8b53ac002a
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-06-05 19:39:56 +0200

    help

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,62 +1,21 @@
-import Notiflix from 'notiflix';
-
-// import SlimSelect from 'slim-select'
-
-// const slimSelect = new SlimSelect({
-//     select: '.breed-select',
-//     events: { afterChange: onSelectCat},
-// });
-
-
-
-const breedSelect = document.querySelector('.breed-select');
-const catInfo = document.querySelector('.cat-info');
-const loaderMess = document.querySelector('.loader');
-// const errorMess = document.querySelector('.error');
 
 
 
 function fetchBreeds() {
-    fetch("https://api.thecatapi.com/v1/breeds")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        }
-        ).then(breeds => {
-          renderBreed(breeds);
-          
-        }).catch((error) => {
-          console.log(error);
-          errorShow();
-})
-      .finally(() => {
-    
-    });
-};
-
-function renderBreed(breeds) {
-  const markup = breeds
-    .map((breed) => {
-        return `
-      <option value="${breed.id}">${breed.name}</option>
-      `;
+  return fetch("https://api.thecatapi.com/v1/breeds")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
     })
-    .join("");
-    
-  // markup.unshift({
-  //       placeholder: true,
-  //       value: 'none',
-  //       text: 'make your selection',
-  //     });
-  // slimSelect.setData(markup);
-  breedSelect.innerHTML = markup;
 }
 
-function fetchPosts() {
+
+
+function fetchPosts(breedId) {
   const searchParams = new URLSearchParams({
-  breed_ids: "beng",
+  breed_ids: breedId,
   api_key: "live_R01pkXHrQNXKQvYeJccUarYSnPmiUXkBylskayNCHe0VBB6reFt08aZBMVBfbdKf",
   });
   const urlCat = `https://api.thecatapi.com/v1/images/search?${searchParams}`;
@@ -65,53 +24,12 @@ function fetchPosts() {
         .then((response) => {
       if (!response.ok) {
         throw new Error(response.status);
-      }
+          }
       return response.json();
     }
-  );
+    )
 }
 
-function fetchCatByBreed(breedId) {
-  const markup = breedId
-      .map(({ url, breeds }) => {
-          for (const breed of breeds) {
-              const cat = `
-      <img src="${url}" alt="${breed.name}" width="300"/>
-      <div> <b>${breed.name}</b>
-      <p>${breed.description} </p>
-      <p> <b>Temperament:</b> ${breed.temperament}   </p>
-      </div>`;
-              return cat;
-        }
-    })
-    .join("");
-  
-  catInfo.innerHTML = markup;
-}
 
-function onSelectCat() {
-  loaderMess.classList.remove('hide');
-  catInfo.classList.add('hide');
-  catInfo.innerHTML = '';
-  setTimeout(() => {
-  fetchPosts()
-    .then((breedId) => {
-      fetchCatByBreed(breedId);
-        })
-      .catch((error) => {
-        console.log(error);
-        errorShow();
-      })
-    .finally(() => {
-      catInfo.classList.remove('hide');
-      loaderMess.classList.add('hide');
-      
-    }) 
-}, 500) 
-}
-
-function errorShow() {
-  Notify.failure('Oops! Something went wrong! Try reloading the page!');
-}
-export { fetchBreeds, onSelectCat, breedSelect }
+export { fetchBreeds, fetchPosts }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { fetchBreeds, onSelectCat, breedSelect  } from "./cat-api";
+import { fetchBreeds, fetchPosts } from "./cat-api";
+// import Notiflix from 'notiflix';
 // import SlimSelect from 'slim-select'
 
 // const slimSelect = new SlimSelect({
@@ -8,13 +9,90 @@ import { fetchBreeds, onSelectCat, breedSelect  } from "./cat-api";
 
 const loaderMess = document.querySelector('.loader');
 const errorMess = document.querySelector('.error');
+const breedSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+
 
 loaderMess.classList.add('hide');
 errorMess.classList.add('hide');
 
+selectBreeds();
 
-fetchBreeds();
+function selectBreeds() {
+    fetchBreeds()
+        .then((breeds) => {
+            console.log(breeds);
+            renderBreed(breeds);
+            
+        }).catch((error) => {
+          console.log(error);
+          errorShow();
+})
+      .finally(() => {
+    
+    });
+};
+
+function renderBreed(breeds) {
+    const markup = breeds
+        .map((breed) => {
+            return `
+      <option value="${breed.id}">${breed.name}</option>
+      `;
+        })
+        .join("");
+    // slimSelect.setData(markup);
+    breedSelect.innerHTML = markup;
+}
+
 
 breedSelect.addEventListener('change', onSelectCat)
 
+function onSelectCat(val) {
+  loaderMess.classList.remove('hide');
+  catInfo.classList.add('hide');
+    catInfo.innerHTML = '';
 
+    console.log(val[0]);
+    
+    breedId = 'abys';
+  
+  fetchPosts(breedId)
+    .then((breedId) => {
+        console.log(breedId);
+      fetchCatByBreed(breedId);
+        })
+      .catch((error) => {
+        console.log(error);
+        errorShow();
+      })
+    .finally(() => {
+      catInfo.classList.remove('hide');
+      loaderMess.classList.add('hide');
+      
+    }) 
+}
+
+function fetchCatByBreed(breedId) {
+  const markup = breedId
+      .map(({ url, breeds }) => {
+          for (const breed of breeds) {
+              const cat = `
+      <img src="${url}" alt="${breed.name}" width="300"/>
+      <div> <b>${breed.name}</b>
+      <p>${breed.description} </p>
+      <p> <b>Temperament:</b> ${breed.temperament}   </p>
+      </div>`;
+              return cat;
+        }
+    })
+    .join("");
+  
+  catInfo.innerHTML = markup;
+}
+
+
+
+function errorShow() {
+  Notify.failure('Oops! Something went wrong! Try reloading the page!');
+}
